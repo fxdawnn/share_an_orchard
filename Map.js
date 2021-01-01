9286587da300e94c54152a77b6b95a9b745cc2f3
fxdawmm <fxdawn@hotmail.com>
2021-01-01 12:11:08 -0800

    Resizing the map size

--- Map.js
+++ Map.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {StyleSheet, Dimensions, View} from 'react-native';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 
 export default class Map extends Component {
   render() {
@@ -16,7 +16,7 @@ export default class Map extends Component {
                     latitudeDelta: 0.0922,
                     longitudeDelta: 0.0421,
                 }}
-            />
+            ></MapView>
         </View>
 
     );
