ab4cfb340e61216ff1f40630db0dcffffc898397
fxdawmm <fxdawn@hotmail.com>
2021-01-01 21:51:37 -0800

    Add mark

--- Map.js
+++ Map.js
@@ -1,6 +1,26 @@
 import React, {Component} from 'react';
 import {StyleSheet, Dimensions, View} from 'react-native';
 import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
+var markers = [
+    {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        title: 'Foo Place',
+        subtitle: '1234 Foo Drive'
+    },
+    {
+        latitude: 37.78824,
+        longitude: -122.4324,
+        title: 'Foo Place',
+        subtitle: '1234 Foo Drive'
+    },
+    {
+        latitude: 37.78823,
+        longitude: -122.4324,
+        title: 'Foo Place',
+        subtitle: '1234 Foo Drive'
+    }
+];
 
 export default class Map extends Component {
   render() {
@@ -16,21 +36,30 @@ export default class Map extends Component {
                     latitudeDelta: 0.0922,
                     longitudeDelta: 0.0421,
                 }}
-            ></MapView>
+                annotations={markers}
+            >
+                <MapView.Marker
+                    coordinate={{latitude: 37.78825,
+                        longitude: -122.4324}}
+                    title={"title"}
+                    description={"description"}
+                />
+            </MapView>
         </View>
 
     );
   }
 }
 
+
 const imageWidth = Dimensions.get("window").width;
 const styles = StyleSheet.create({
     mapContainer: {
-        height: imageWidth * (2 / 3),
+        height: imageWidth,
         backgroundColor: '#43AA8B'
     },
     map: {
         flex: 1,
-        height: imageWidth * (2 / 3)
+        height: imageWidth
     }
 });
