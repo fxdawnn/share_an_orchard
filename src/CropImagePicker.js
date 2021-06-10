31d1b7ad81f70127efdb0c3f84ac03a0d941d359
fxdawmm <fxdawn@hotmail.com>
2021-06-10 16:03:09 -0700

    Add crop camera pickers and photos selectiosn

--- src/CropImagePicker.js
+++ src/CropImagePicker.js
@@ -1,48 +1,211 @@
 import React, {useState} from 'react';
-import {View, Text, TouchableOpacity, Alert} from 'react-native';
-// import {ImagePicker, launchCamera, launchImageLibrary} from 'react-native-image-picker';
-import * as ImagePicker from 'react-native-image-picker';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Alert,
+  PermissionsAndroid,
+  SafeAreaView,
+  StyleSheet,
+  Image,
+  Platform,
+} from 'react-native';
+import {
+  ImagePicker,
+  launchCamera,
+  launchImageLibrary,
+} from 'react-native-image-picker';
+//import * as ImagePicker from 'react-native-image-picker';
 
 export default function SimpleImagePicker() {
   const [imageSource, setImageSource] = useState(null);
+  const [filePath, setFilePath] = useState({});
+  const requestCameraPermission = async () => {
+    if (Platform.OS === 'android') {
+      try {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.CAMERA,
+          {
+            title: 'Camera Permission',
+            message: 'App needs camera permission',
+          },
+        );
+        // If CAMERA Permission is granted
+        return granted === PermissionsAndroid.RESULTS.GRANTED;
+      } catch (err) {
+        console.warn(err);
+        return false;
+      }
+    } else {
+      return true;
+    }
+  };
 
-  function selectImage() {
+  const requestExternalWritePermission = async () => {
+    if (Platform.OS === 'android') {
+      try {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+          {
+            title: 'External Storage Write Permission',
+            message: 'App needs write permission',
+          },
+        );
+        // If WRITE_EXTERNAL_STORAGE Permission is granted
+        return granted === PermissionsAndroid.RESULTS.GRANTED;
+      } catch (err) {
+        console.warn(err);
+        alert('Write permission err', err);
+      }
+      return false;
+    } else {
+      return true;
+    }
+  };
+  const captureImage = async (type) => {
     let options = {
-      title: 'You can choose one image',
-      maxWidth: 256,
-      maxHeight: 256,
-      storageOptions: {
-        skipBackup: true,
-      },
+      mediaType: type,
+      maxWidth: 300,
+      maxHeight: 550,
+      quality: 1,
+      videoQuality: 'low',
+      durationLimit: 30, //Video max duration in seconds
+      saveToPhotos: true,
     };
+    let isCameraPermitted = await requestCameraPermission();
+    let isStoragePermitted = await requestExternalWritePermission();
+    if (isCameraPermitted && isStoragePermitted) {
+      launchCamera(options, (response) => {
+        console.log('Response = ', response);
 
-    ImagePicker.showImagePicker(options, (response) => {
-      console.log({response});
+        if (response.didCancel) {
+          alert('User cancelled camera picker');
+          return;
+        } else if (response.errorCode == 'camera_unavailable') {
+          alert('Camera not available on device');
+          return;
+        } else if (response.errorCode == 'permission') {
+          alert('Permission not satisfied');
+          return;
+        } else if (response.errorCode == 'others') {
+          alert(response.errorMessage);
+          return;
+        }
+        console.log('base64 -> ', response.base64);
+        console.log('uri -> ', response.uri);
+        console.log('width -> ', response.width);
+        console.log('height -> ', response.height);
+        console.log('fileSize -> ', response.fileSize);
+        console.log('type -> ', response.type);
+        console.log('fileName -> ', response.fileName);
+        setFilePath(response);
+      });
+    }
+  };
+
+  const chooseFile = (type) => {
+    let options = {
+      mediaType: type,
+      maxWidth: 300,
+      maxHeight: 550,
+      quality: 1,
+    };
+    launchImageLibrary(options, (response) => {
+      console.log('Response = ', response);
 
       if (response.didCancel) {
-        console.log('User cancelled photo picker');
-        Alert.alert('You did not select any image');
-      } else if (response.error) {
-        console.log('ImagePicker Error: ', response.error);
-      } else if (response.customButton) {
-        console.log('User tapped custom button: ', response.customButton);
-      } else {
-        let source = {uri: response.uri};
-        console.log({source});
+        alert('User cancelled camera picker');
+        return;
+      } else if (response.errorCode == 'camera_unavailable') {
+        alert('Camera not available on device');
+        return;
+      } else if (response.errorCode == 'permission') {
+        alert('Permission not satisfied');
+        return;
+      } else if (response.errorCode == 'others') {
+        alert(response.errorMessage);
+        return;
       }
+      console.log('base64 -> ', response.base64);
+      console.log('uri -> ', response.uri);
+      console.log('width -> ', response.width);
+      console.log('height -> ', response.height);
+      console.log('fileSize -> ', response.fileSize);
+      console.log('type -> ', response.type);
+      console.log('fileName -> ', response.fileName);
+      setFilePath(response);
     });
-  }
+  };
 
   return (
-    <View>
-      <Text>
-        Simple Image Picker
+    <SafeAreaView style={{flex: 1}}>
+      <Text style={styles.titleText}>
+        Choose or take photos of the crops
       </Text>
-      <TouchableOpacity
-        onPress={selectImage}
-        >
-        <Text>Pick an image</Text>
-      </TouchableOpacity>
-    </View>
+      <View style={styles.container}>
+        {/* <Image
+          source={{
+            uri: 'data:image/jpeg;base64,' + filePath.data,
+          }}
+          style={styles.imageStyle}
+        /> */}
+        <Image source={{uri: filePath.uri}} style={styles.imageStyle} />
+        <Text style={styles.textStyle}>{filePath.uri}</Text>
+        <TouchableOpacity
+          activeOpacity={0.5}
+          style={styles.secondaryButton}
+          onPress={() => captureImage('photo')}>
+          <Text style={styles.secondaryButtonText}>
+            Launch Camera for Image
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          activeOpacity={0.5}
+          style={styles.secondaryButton}
+          onPress={() => captureImage('video')}>
+          <Text style={styles.secondaryButtonText}>
+            Launch Camera for Video
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          activeOpacity={0.5}
+          style={styles.secondaryButton}
+          onPress={() => chooseFile('photo')}>
+          <Text style={styles.secondaryButtonText}>Choose Image</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          activeOpacity={0.5}
+          style={styles.secondaryButton}
+          onPress={() => chooseFile('video')}>
+          <Text style={styles.secondaryButtonText}>Choose Video</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
+const styles = StyleSheet.create({
+  secondaryButton: {
+    /* Share an orchard */
+
+    position: 'relative',
+    width: 315.77,
+    height: 51.83,
+    /*left: 32.62,
+                    top: 630.17,*/
+    backgroundColor: '#43aa8b',
+    borderRadius: 22,
+    borderWidth: 3,
+    borderColor: '#DD5252',
+
+    /*filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));*/
+  },
+  secondaryButtonText: {
+    fontFamily: 'Red Hat Display',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: 34,
+    lineHeight: 45,
+    textAlign: 'center',
+    color: '#254441',
+  },
+});
